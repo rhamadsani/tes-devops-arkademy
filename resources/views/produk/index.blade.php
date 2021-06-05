093e06d35bf629d5de59b4797ec5fbcf1352e6e9
Rhamad Nursani <rhamadsani@gmail.com>
2021-06-05 14:42:13 -0700

    implent api single page

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -43,8 +43,23 @@
     .custom-button{
         border: solid 2px #757575;
         cursor: pointer;
-        padding: 5px;
+        padding: 6px;
         border-radius: 5px;
+    }
+    .btn-success{
+        color: white;
+        background: #009879;
+        border: solid 2px #009879;
+    }
+    .btn-warning{
+        color: white;
+        background: #df713e;
+        border: solid 2px #df713e;
+    }
+    .btn-danger{
+        color: white;
+        background: #ff7043;
+        border: solid 2px #ff7043;
     }
     </style>
 </head>
@@ -54,51 +69,79 @@
             <thead>
                 <th>Nama Baran</th>
                 <th>Keterangan</th>
-                <th>Harga</th>
-                <th>Jumlah</th>
+                <th>Harga (Rp)</th>
+                <th>Jumlah (Buah)</th>
                 <th>Action</th>
             </thead>
             <tbody id="table-body">
                 @foreach($produk as $data)
-                <tr>
+                <tr id="tr-{{$data->id ?? 0}}">
                     <td>{{$data->nama_produk ?? ''}}</td>
                     <td>{{$data->keterangan ?? ''}}</td>
                     <td>{{$data->harga ?? ''}}</td>
                     <td>{{$data->jumlah ?? ''}}</td>
                     <td>
-                        <a class="custom-button" onclick="editProduk({{$data->id ?? 0}},`{{$data->nama_produk ?? ''}}`, `{{$data->keterangan ?? ''}}`,{{$data->harga ?? 0}},{{$data->jumlah ?? 0}})">Edit</a>
-                        <a class="custom-button" onclick="deleteProduk({{$data->id ?? 0}})">Delete</a>
+                        <a class="custom-button btn-warning" onclick="editProduk({{$data->id ?? 0}},`{{$data->nama_produk ?? ''}}`, `{{$data->keterangan ?? ''}}`,{{$data->harga ?? 0}},{{$data->jumlah ?? 0}})">Edit</a>
+                        <a class="custom-button btn-danger" onclick="deleteProduk({{$data->id ?? 0}})">Delete</a>
                     </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
         <div>
-            <a class="custom-button" onclick="addProduk()">Tambah Produk</a>
+            <a class="custom-button btn-success" onclick="addProduk()">Tambah Produk</a>
         </div>
     </div>
 </body>
 <script src="sweetalert.js"></script>
 <script src="jquery.js"></script>
 <script>
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     async function addProduk()
     {
         const { value: formValues } = await Swal.fire({
         title: 'Tambah Produk',
+        confirmButtonText:'Simpan',
         html:
-            '<input id="swal-input1" type="text" class="swal2-input">' +
-            '<input id="swal-input2" type="text" class="swal2-input">'+
-            '<input id="swal-input3" type="number" class="swal2-input">'+
-            '<input id="swal-input4" type="number" class="swal2-input">',
+            '<input id="swal-input1" placeholder="Nama Produk" type="text" class="swal2-input">' +
+            '<input id="swal-input2" placeholder="Keterangan Produk" type="text" class="swal2-input">'+
+            '<input id="swal-input3" placeholder="Harga Produk" type="number" class="swal2-input">'+
+            '<input id="swal-input4" placeholder="Jumlah Produk" type="number" class="swal2-input">',
         focusConfirm: false,
         preConfirm: () => {
-            return [
-            document.getElementById('swal-input1').value,
-            document.getElementById('swal-input2').value,
-            document.getElementById('swal-input3').value,
-            document.getElementById('swal-input4').value,
-            ]
-        }
+                return new Promise((resolve, reject) => {
+                    let a = document.getElementById('swal-input1').value;
+                    let b = document.getElementById('swal-input2').value;
+                    let c = document.getElementById('swal-input3').value;
+                    let d = document.getElementById('swal-input4').value;
+                    if(a == ''){
+                        reject('Nama Produk')
+                    }else if(b == ''){
+                        reject('Keterangan Produk')
+                    }else if(c == ''){
+                        reject('Harga Produk')
+                    }else if(d == ''){
+                        reject('Jumlah Produk')
+                    }else{
+                        resolve([a,b,c,d]);
+                    }
+                    }).catch(error => {
+                    Swal.showValidationMessage(
+                    `${error}`
+                    )
+                })
+            },
         })
 
         if (formValues) {
@@ -118,9 +161,13 @@
                     html+='<td>'+result.keterangan+'</td>';
                     html+='<td>'+result.harga+'</td>';
                     html+='<td>'+result.jumlah+'</td>';
-                    html+='<td><a class="custom-button" onclick="editProduk({{$data->id ?? 0}},`{{$data->nama_produk ?? ''}}`, `{{$data->keterangan ?? ''}}`,{{$data->harga ?? 0}},{{$data->jumlah ?? 0}})">Edit</a>';
-                    html+='<a class="custom-button" onclick="deleteProduk({{$data->id ?? 0}})">Delete</a></td></tr>';
+                    html+='<td><a class="custom-button" onclick="editProduk('+result.id+',`'+result.nama_produk+'`, `'+result.keterangan+'`,'+result.harga+','+result.jumlah+')">Edit</a>';
+                    html+='<a class="custom-button" onclick="deleteProduk('+result.id+')">Delete</a></td></tr>';
                     $("#table-body").prepend(html)
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Produk ditambahkan'
+                    })
                 }
             })
         }
@@ -129,12 +176,12 @@
     async function editProduk(id,nama, keterangan, harga, jumlah)
     {
         const { value: formValues } = await Swal.fire({
-        title: 'Tambah Produk',
+        title: 'Ubah Produk',
         html:
             '<input id="edit-input1" type="text" class="swal2-input" value="'+nama+'">' +
             '<input id="edit-input2" type="text" class="swal2-input" value="'+keterangan+'">'+
-            '<input id="edit-input3" type="number" class="swal2-input" value="'+jumlah+'">'+
-            '<input id="edit-input4" type="number" class="swal2-input" value="'+harga+'">',
+            '<input id="edit-input3" type="number" class="swal2-input" value="'+harga+'">'+
+            '<input id="edit-input4" type="number" class="swal2-input" value="'+jumlah+'">',
         focusConfirm: false,
         preConfirm: () => {
             return [
@@ -158,6 +205,22 @@
                     harga:formValues[2],
                     jumlah:formValues[3]
                 },
+                success:function(result)
+                {
+                    let html = '';
+                    html+='<td>'+result.nama_produk+'</td>';
+                    html+='<td>'+result.keterangan+'</td>';
+                    html+='<td>'+result.harga+'</td>';
+                    html+='<td>'+result.jumlah+'</td>';
+                    html+='<td><a class="custom-button" onclick="editProduk('+result.id+',`'+result.nama_produk+'`, `'+result.keterangan+'`,'+result.harga+','+result.jumlah+')">Edit</a>';
+                    html+='<a class="custom-button" onclick="deleteProduk('+result.id+')">Delete</a></td>';
+                    $("#tr-"+id).html(html)
+
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Produk diubah'
+                    })
+                }
             })
         }
     }
@@ -182,6 +245,9 @@
                     _token:'{{csrf_token()}}',
                     _method:'DELETE',
                 },
+                success:function(){
+                    $("#tr-"+id).remove();
+                }
             })
         }
         })
